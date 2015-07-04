5b28c4ac1565264dcab6bbafe2257cb491a3842e
akovski <akovskiboy@gmail.com>
2015-07-05 00:47:52 +0800

    modular inverse added

--- gcd.h
+++ gcd.h
@@ -1,8 +1,26 @@
 #include "bigInt.h"
 
-bigInt gcd(const bigInt& a, const bigInt& b) {
+bigInt gcd(bigInt a, bigInt b) {
     if (a > b) return gcd(b, a);
     if (a == bigInt("0")) return b;
     return gcd(b % a, a);
+}
+
+void extendedEuclidean(bigInt a, bigInt b, bigInt &x, bigInt &y, const bigInt& m) {
+    if (a == bigInt("0")) {
+        x = bigInt("0"), y = bigInt("1");
+        return;
+    }
+    bigInt c = b / a, d = b % a;
+    extendedEuclidean(d, a, y, x, m);
+    x = (x + m - (c * y) % m) % m;  //to prevent an sub overflow
+}
+
+bigInt inv(bigInt a, bigInt mod) {
+    if (!(gcd(a, mod) == bigInt("1"))) return bigInt("0");
+    bigInt x, y;
+    a = a % mod;
+    extendedEuclidean(a, mod, x, y, mod);
+    return x;
 }
 
