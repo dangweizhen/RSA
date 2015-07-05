49a44a8ba31224490b0ad3166b1849eab7bfadcd
akovski <akovskiboy@gmail.com>
2015-07-05 22:33:31 +0800

    add random bigInt generator

---
+++ random.h
@@ -0,0 +1,24 @@
+#include "bigInt.h"
+#include "power.h"
+#include <ctime>
+#include <cstdlib>
+
+void setRandom(unsigned seed) {
+    srand(seed);
+}
+
+
+bigInt random(int len) {
+    if (len == 0) return bigInt("0");
+    if (len == 1) return bigInt("1");
+    bigInt x("1");
+    for (int i = 1; i < len; i ++) x = x * bigInt("2");;
+    string s(len, '0');
+    for (int i = 0; i < s.length(); i ++) 
+        s[i] += rand() % 10;
+    bigInt res = bigInt(s) % x + x;
+    if (res % bigInt("2") == bigInt("0")) 
+        return res + bigInt("1");
+    else
+        return res;
+}
--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include "bigInt.h"
 #include "power.h"
 #include "gcd.h"
+#include "random.h"
 using namespace std;
 int main() {
     cout << power(bigInt("53223232"), bigInt("432043294032"), bigInt("323232143243478394738")) << endl;
@@ -8,4 +9,6 @@ int main() {
     cout << inv(bigInt("3"), bigInt("10")) << endl;
     cout << inv(bigInt("3234476509624757991344647769100216810857203198904625400933895331391691459636928060001"), 
                 bigInt("265613988875874769338781322035779626829233452653394495974574961739092490901302182994384699044001")) << endl;
+    cout << random(10) << endl;
+    for (int i = 0; i < 10; i ++) cout << random(512).getnum() << endl;
 }
